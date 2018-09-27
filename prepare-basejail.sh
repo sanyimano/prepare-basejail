39812a23cedc850d982fa8100dde34c500db10c5
sanyimano <berta@beco.hu>
2018-09-27 13:17:40 +0200

    Update prepare-basejail.sh
    When I download the package master.zip file, the unzip extracts to prepare-basejail-master directory, not to /root as happened previously.
    I changed the abslout path to relative in line 22,23.

--- prepare-basejail.sh
+++ prepare-basejail.sh
@@ -3,9 +3,9 @@ sysrc jail_enable=YES
 sysrc zfs_enable=YES
 pkg install pkg wget
 pkg install unzip
-JAILPOOL=zdata
-FREEBSDVERSION='11.2-RELEASE'
+JAILPOOL="zdata"
 JAILROOT="/mnt/zdata"
+FREEBSDVERSION='11.2-RELEASE'
 zfs create $JAILPOOL/.my_jails_cache
 cd $JAILROOT/.my_jails_cache/
 wget http://ftp1.us.freebsd.org/pub/FreeBSD/releases/amd64/$FREEBSDVERSION/base.txz
@@ -19,7 +19,7 @@ echo "resolve localtime és hosts beállítás"
 cp /etc/resolv.conf $JAILROOT/.my_jails_cache/etc/
 cp /etc/localtime $JAILROOT/.my_jails_cache/etc/
 cp /etc/hosts $JAILROOT/.my_jails_cache/etc/
-cp /root/prepare-inside.sh $JAILROOT/.my_jails_cache/root
-cp /root/jail.conf /etc/
+cp prepare-inside.sh $JAILROOT/.my_jails_cache/root
+cp jail.conf /etc/
 service jail start
 
